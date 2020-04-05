e56a887b828a65bb9750a16a644d0be74b59e557
luowei2018 <42980027+luowei2018@users.noreply.github.com>
2020-04-05 14:33:49 -0500

    Add checking features

--- Web Design/main.py
+++ Web Design/main.py
@@ -9,7 +9,15 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
-    
+
+@app.route("/check")
+def check():
+    return render_template("check.html")
+
+@app.route("/result")
+def result():
+    return render_template("result.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
- 
+
